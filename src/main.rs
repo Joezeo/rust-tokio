f31ec6a6dc0dbef5924b39eee050b822a6bccc9b
JoeZane <joezane.cn@gmail.com>
2023-01-01 23:51:57 +0800

    1. add code about redis server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use mini_redis::{Connection, Frame, Command, Command::{Set, Get}, };
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
+
+    // Connection was the abstract of redis reading/writing process
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("Got: {:?}", frame);
+
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("Ok".to_string())
+            },
+            Get(cmd) => {
+                if let Some(val) = db.get(cmd.key()) {
+                    Frame::Bulk(val.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            _ => unimplemented!()
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
 }
